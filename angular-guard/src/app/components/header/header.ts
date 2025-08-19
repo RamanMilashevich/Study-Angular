5863ac4172ca8106dec6cdaa1bcf0f94294dd60b
RamanMilashevich <roma.milashevich@gmail.com>
2025-08-19 16:34:01 +0200

    feat: Subjects & Multicasting

--- angular-guard/src/app/components/header/header.ts
+++ angular-guard/src/app/components/header/header.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { of, interval, from, Subject, ReplaySubject} from 'rxjs';
+import { of, interval, from, Subject, ReplaySubject, BehaviorSubject, AsyncSubject} from 'rxjs';
 
 @Component({
   selector: 'app-header',
@@ -23,20 +23,40 @@ export class HeaderComponent {
     // // Stop after 5s
     // setTimeout(() => sub.unsubscribe(), 5000);
 
-    const subject = new Subject<number>();
+    // const subject = new Subject<number>();
 
-    subject.subscribe(v => console.log('Observer first:', v))
-    subject.subscribe(v => console.log('Observer second:', v))
+    // subject.subscribe(v => console.log('Observer first:', v))
+    // subject.subscribe(v => console.log('Observer second:', v))
 
-    subject.next(1);
-    subject.next(2);
+    // subject.next(1);
+    // subject.next(2);
 
 
-    const replay$ = new ReplaySubject<number>(2); // buffer size 2; return the late subscribers
-    replay$.next(1);
-    replay$.next(2);
-    replay$.next(3);
-    replay$.next(5);
-    replay$.subscribe(v => console.log(v))
+    // const replay$ = new ReplaySubject<number>(2); // buffer size 2; return the late subscribers
+    // replay$.next(1);
+    // replay$.next(2);
+    // replay$.next(3);
+    // replay$.next(5);
+    // replay$.subscribe(v => console.log(v))
+
+
+    // const behavior$ = new BehaviorSubject<string>('Hello'); // Always has a current value, delivered immediately to new subscribers.
+
+    // behavior$.subscribe(v=> console.log('A:', v))
+    // behavior$.next('world!')
+    // behavior$.subscribe(v => console.log("B:", v)) // world!
+
+
+    const async$ = new AsyncSubject<number>(); // Emits only the last value when completed.
+
+    async$.subscribe(v=> console.log('Sub1:', v))
+
+    async$.next(1);
+    async$.next(2);
+    async$.next(3);
+
+    async$.subscribe(v => console.log('Sub2:', v))
+
+    async$.complete() //Emits only the last value when completed. // 3
   }
 }
