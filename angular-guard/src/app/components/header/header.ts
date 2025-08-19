c67d979e7ceef0aeab6b56d36f2417fcc6b3e6f3
RamanMilashevich <roma.milashevich@gmail.com>
2025-08-19 16:20:00 +0200

    feat: add ReplaySubject test

--- angular-guard/src/app/components/header/header.ts
+++ angular-guard/src/app/components/header/header.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { of, interval,  } from 'rxjs';
+import { of, interval, from, Subject, ReplaySubject} from 'rxjs';
 
 @Component({
   selector: 'app-header',
@@ -12,14 +12,31 @@ export class HeaderComponent {
 
   constructor() {
     // Example 1: simple observable
-    const source$ = of(1, 2, 3); // emits 1,2,3 then completes
-    source$.subscribe(value=> console.log(value));
+    // const source$ = from(['A', 'B', 'C']); // emits 1,2,3 then completes
+    // source$.subscribe(value=> console.log(value));
 
-    // Example 2: interval
-    const timer$ = interval(1000); // emits 0,1,2,... every 1s
-    const sub = timer$.subscribe(v => console.log(v));
 
-    // Stop after 5s
-    setTimeout(() => sub.unsubscribe(), 5000);
+    // // Example 2: interval
+    // const timer$ = interval(1000); // emits 0,1,2,... every 1s
+    // const sub = timer$.subscribe(v => console.log(v));
+
+    // // Stop after 5s
+    // setTimeout(() => sub.unsubscribe(), 5000);
+
+    const subject = new Subject<number>();
+
+    subject.subscribe(v => console.log('Observer first:', v))
+    subject.subscribe(v => console.log('Observer second:', v))
+
+    subject.next(1);
+    subject.next(2);
+
+
+    const replay$ = new ReplaySubject<number>(2); // buffer size 2; return the late subscribers
+    replay$.next(1);
+    replay$.next(2);
+    replay$.next(3);
+    replay$.next(5);
+    replay$.subscribe(v => console.log(v))
   }
 }
