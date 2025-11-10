27b5dbcc9527def0d1af3a0772107c973b275ef5
RamanMilashevich <roma.milashevich@gmail.com>
2025-11-10 13:51:36 +0100

    feat: enhance template-driven form with reactive form integration; add validation messages and update form structure

--- angular-guard/src/app/homework/templatedriven-form/templatedriven-form.ts
+++ angular-guard/src/app/homework/templatedriven-form/templatedriven-form.ts
@@ -1,11 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
-import { FormsModule, NgForm } from '@angular/forms';
+import { FormControl, FormGroup, FormsModule, NgForm, Validators, ReactiveFormsModule } from '@angular/forms';
 
 
 @Component({
   selector: 'app-templatedriven-form',
-  imports: [FormsModule, CommonModule],
+  imports: [FormsModule, CommonModule, ReactiveFormsModule],
   templateUrl: './templatedriven-form.html',
   styleUrl: './templatedriven-form.css'
 })
@@ -21,5 +21,15 @@ export class TemplatedrivenForm {
 
   onSubmit(form: NgForm) {
     console.log('[Template Driven form]: ', form.value)
+  }
+
+  myReactiveForm = new FormGroup({
+    userName: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    userSurname: new FormControl('', [Validators.required, Validators.minLength(4)]),
+    password: new FormControl('', [Validators.required, Validators.minLength(3)])
+  })
+
+  doSomething(form: FormGroup) {
+    console.log(form)
   }
 }
